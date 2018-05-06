e8e4f2acf28e3dfb2ca7267cad7a9f56319a14e1
Anton Zhukov <zhukov1337@gmail.com>
2018-05-07 01:23:03 +0200

    Remove useless map in userProvider

--- hub/users.go
+++ hub/users.go
@@ -7,14 +7,12 @@ type UserProvider interface {
 }
 
 type Users struct {
-	users           map[int32]struct{}
 	availableUserID int32
 	lock            sync.RWMutex
 }
 
 func NewUsers() *Users {
 	return &Users{
-		users:           make(map[int32]struct{}),
 		availableUserID: 1,
 	}
 }
@@ -22,7 +20,6 @@ func NewUsers() *Users {
 func (u *Users) AuthenticateNewUser() int32 {
 	u.lock.Lock()
 	id := u.availableUserID
-	u.users[u.availableUserID] = struct{}{}
 	u.availableUserID++
 	u.lock.Unlock()
 	return id
